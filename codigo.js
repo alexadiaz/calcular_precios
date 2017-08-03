e74fcb0c4b63cbe9a87e086991b15f31e1e51d1a
Alexa <alexadiaz@msn.com>
2017-08-02 21:30:54 -0400

    validaciones

--- codigo.js
+++ codigo.js
@@ -16,10 +16,32 @@ function comenzar() {
   console.dir(datos);
 
   consultar.addEventListener("click", function() {
+    if (pais.value === "") {
+      costo_envio.textContent = "Debe ingresar el pais";
+      return;
+    }
+    if (ciudad.value === "") {
+      costo_envio.textContent = "Debe ingresar la ciudad";
+      return;
+    }
+    if (cantidad.value === "") {
+      costo_envio.textContent = "Debe ingresar cantidad";
+      return;
+    }
     var id_pais = datos.getCountrySync(pais.value);
     console.log(id_pais);
+    if (id_pais === null) {
+      costo_envio.textContent = "Pais no existe";
+      return;
+    }
+
     var id_city = datos.getCitySync(id_pais, ciudad.value);
     console.log(id_city);
+    if (id_city === null) {
+      costo_envio.textContent = "Cuidad no existe";
+      return;
+    }
+
     var price = datos.getPriceSync(id_city);
     console.log(price);
     costo_envio.textContent = parseInt(cantidad.value) * price;
