f0e05675133c04dd664dcfb2759509bff92ad797
Alexa <alexadiaz@msn.com>
2017-08-03 15:43:10 -0400

    color mensajes, mensaje de espera, deshabilitar botones en busqueda

--- codigo.js
+++ codigo.js
@@ -16,33 +16,54 @@ function comenzar() {
   console.dir(datos);
 
   consultar.addEventListener("click", function() {
+    costo_envio.style.color = "black";
+    costo_envio.textContent = "Espere ...";
+
     if (pais.value === "") {
+      costo_envio.style.color = "red";
       costo_envio.textContent = "Debe ingresar el pais";
       return;
     }
     if (ciudad.value === "") {
+      costo_envio.style.color = "red";
       costo_envio.textContent = "Debe ingresar la ciudad";
       return;
     }
     if (cantidad.value === "") {
+      costo_envio.style.color = "red";
       costo_envio.textContent = "Debe ingresar cantidad";
       return;
     }
 
+    botones(pais, ciudad, cantidad, consultar, true);
+
     datos.getCountryCallback(pais.value, function(error, id_pais) {
       if (error !== null) {
+        costo_envio.style.color = "blue";
         costo_envio.textContent = "Pais no existe";
+        botones(pais, ciudad, cantidad, consultar, false);
         return;
       }
       datos.getCityCallback(id_pais, ciudad.value, function(error, id_city) {
         if (error !== null) {
+          costo_envio.style.color = "blue";
           costo_envio.textContent = "Ciudad no existe";
+          botones(pais, ciudad, cantidad, consultar, false);
           return;
         }
         datos.getPriceCallback(id_city, function(error, price) {
+          costo_envio.style.color = "black";
           costo_envio.textContent = price * parseInt(cantidad.value, 10);
+          botones(pais, ciudad, cantidad, consultar, false);
         });
       });
     });
   });
+}
+
+function botones(pais, ciudad, cantidad, consultar, estado) {
+  pais.disabled = estado;
+  ciudad.disabled = estado;
+  cantidad.disabled = estado;
+  consultar.disabled = estado;
 }
